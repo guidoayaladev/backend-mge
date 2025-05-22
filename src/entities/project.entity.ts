859ac531a2a03e250711f2506dcba967cf80f1ef
guidoayaladev <guidoayaladev@gmail.com>
2025-05-22 15:32:56 -0500

    Fix: Relaciones Ajustes projects

--- src/entities/project.entity.ts
+++ src/entities/project.entity.ts
@@ -14,7 +14,7 @@ export class Project {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({ unique: true })
   name: string;
 
   @Column()
--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from 'src/entities/project.entity';
 import { ProjectController } from './controller/projects.controller';
 import { ProjectService } from './service/projects.service';
+import { UserEntity } from 'src/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [TypeOrmModule.forFeature([Project, UserEntity])],
   controllers: [ProjectController],
   providers: [ProjectService],
 })
