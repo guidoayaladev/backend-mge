f44e851357c712678d19a218ece0d6b9c0a1b7cd
guidoayaladev <guidoayaladev@gmail.com>
2025-05-22 08:47:13 -0500

    Fix: RegEx Dto Login

--- src/modules/auth/dtos/login.dto.ts
+++ src/modules/auth/dtos/login.dto.ts
@@ -1,9 +1,15 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, Matches } from 'class-validator';
 
 export class LoginDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'El correo electrónico no tiene un formato válido' })
   email: string;
 
-  @IsString()
+  @Matches(
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/,
+    {
+      message:
+        'La contraseña debe tener al menos 8 caracteres, una mayúscula, un número y un carácter especial',
+    },
+  )
   password: string;
 }
